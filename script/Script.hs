5c9253b3965286c88e0323fa4e1aa1fbf4f7d007
chrisbrown1982 <chrisbrown.guitar@googlemail.com>
2024-04-17 16:32:53 +0100

    script

--- script/Script.hs
+++ script/Script.hs
@@ -7,14 +7,14 @@ import Data.Maybe
 
 
 calcSeqs [] = [] 
-calcSeqs (l1:l2:lines) = let before = (read l1) :: Float 
-                             after  = (read l2) :: Float 
+calcSeqs (l1:l2:lines) = let before = (read l1) :: Integer 
+                             after  = (read l2) :: Integer
                              time = after - before 
                          in time : calcSeqs lines 
 
 calcPars [] = [] 
-calcPars (_:l1:l2:lines) = let before = (read l1) :: Float 
-                               after  = (read l2) :: Float  
+calcPars (_:l1:l2:lines) = let before = (read l1) :: Integer 
+                               after  = (read l2) :: Integer  
                                time = after - before
                           in time : calcPars lines 
 
@@ -22,7 +22,7 @@ every n xs = case drop (n-1) xs of
               y : ys -> y : every n ys
               [] -> []
 
-every2 c n xs 
+every2 c n base xs 
   | n > 13    = putStrLn ""
   | otherwise = do 
                 let s1 = xs !! n 
@@ -30,21 +30,22 @@ every2 c n xs
                     s3 = xs !! (n + 14 + 14)
                     s4 = xs !! (n + 14 + 14 + 14)
                     s5 = xs !! (n + 14 + 14 + 14 + 14)
-                    res = (s1 + s2 + s3 + s4 + s5) / 5
-                putStrLn $ (show c) ++ " , " ++ show res 
-                every2 (c + 2) (n+1) xs 
+                    res = (fromIntegral (s1 + s2 + s3 + s4 + s5) :: Float) / 5
+                    res2 = base / res
+                putStrLn $ (show c) ++ " \t " ++ show res2 
+                every2 (c + 2) (n+1) base xs 
 
 parseEntries [] = return ""
 parseEntries i = do let (seq, rest) = splitAt 10 i 
                     let seqTimes = calcSeqs seq 
-                    let seqAvgT  = (sum seqTimes) / 5
-                    putStrLn $ "1" ++ " , " ++ (show seqAvgT)
+                    let seqAvgT  = (fromIntegral (sum seqTimes) :: Float) / 5
+                    putStrLn $ "1" ++ " \t " ++ (show seqAvgT)
 
                     let parTimes = calcPars rest
 
                     -- putStrLn (show parTimes)
 
-                    every2 2 0 parTimes 
+                    every2 2 0 seqAvgT parTimes 
 
                     -- putStrLn parTimes2
 
